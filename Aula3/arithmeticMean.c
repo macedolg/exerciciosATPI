5830ac43a84c6594ee6976b7ebea13fd3f50509e
Lucas Gabriel Macêdo <dev.macedolg@gmail.com>
2024-05-26 11:55:40 -0300

    [Finish] Exercicío 2 Aula3.

--- Aula3/arithmeticMean.c
+++ Aula3/arithmeticMean.c
@@ -1,3 +1,30 @@
 //
 // Created by macedolg on 5/26/2024.
 //
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+    double grade1, grade2, grade3, grade4, average;
+    int rounded_average;
+
+    printf("Enter the 1º grade: \n");
+    scanf("%lf", &grade1);
+
+    printf("Enter the 2º grade: \n");
+    scanf("%lf", &grade2);
+
+    printf("Enter the 3º grade: \n");
+    scanf("%lf", &grade3);
+
+    printf("Enter the 4º grade: \n");
+    scanf("%lf", &grade4);
+
+    average = (grade1 + grade2 + grade3 + grade4) / 4.0;
+    rounded_average = round(average);
+
+    printf("The arithmetic mean (rounded) of the grades is: %d\n", rounded_average);
+
+    return 0;
+}
