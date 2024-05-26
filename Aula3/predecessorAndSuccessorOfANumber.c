739351186a8b9fd7cc81bfb954f24902be6aa9e2
Lucas Gabriel Macêdo <dev.macedolg@gmail.com>
2024-05-26 11:58:49 -0300

    [Finish] Exercicío 3 Aula3.

--- Aula3/predecessorAndSuccessorOfANumber.c
+++ Aula3/predecessorAndSuccessorOfANumber.c
@@ -2,3 +2,18 @@
 // Created by macedolg on 5/26/2024.
 //
 
+#include <stdio.h>
+
+int main(void) {
+
+    int n, predecessor, successor;
+
+    printf("Enter a Number: \n");
+    scanf("%d", &n);
+
+    predecessor = n - 1;
+    successor = n + 1;
+
+    printf("\nThe predecessor of %d is %d.\nThe successor of %d is %d.\n", n, predecessor, n, successor);
+    return 0;
+}
