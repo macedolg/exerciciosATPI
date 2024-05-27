9145723f7f158db878538c846389367333cc8bc4
macedolg <dev.macedolg@gmail.com>
2024-05-27 08:37:12 -0300

    [Finish] Exercício 7 Aula 3.

--- Aula3/prizeAwarding.c
+++ Aula3/prizeAwarding.c
@@ -2,3 +2,21 @@
 // Created by macedolg on 27/05/24.
 //
 
+#include <stdio.h>
+
+int main(void) {
+
+    int first, second, third;
+    double awardValue;
+
+    printf("Enter the value of the award: \n$");
+    scanf("%lf", &awardValue);
+
+    first = awardValue * 0.5;
+    second = awardValue * 0.3;
+    third = awardValue * 0.2;
+
+    printf("The values of the top 3 awards are: \n1st: $%d \n2nd: $%d \n3rd: $%d", first, second, third);
+
+    return 0;
+}
