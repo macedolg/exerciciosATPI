c688225fc511111463d0dd6cd2f52f74ee45053d
macedolg <dev.macedolg@gmail.com>
2024-05-27 15:47:58 -0300

    [Finish] Exercício 12 Aula 4.

--- Aula4/powerConsumptionCalculator.c
+++ Aula4/powerConsumptionCalculator.c
@@ -29,17 +29,17 @@ int main(void) {
 
     float spendAmountYear = consumptionYear * priceKWH;
 
-    // float wSpendDaily = (dailyUse * consumptionDay) ;
-    // float wSpendMonthly = (dailyUse * consumptionMonth);
-    // float wSpendYearly = (dailyUse * consumptionYear);
+     float wattsDay = dailyUse * watts;
+     float wattsMonth = wattsDay * 30;
+     float wattsYear = wattsDay * 365;
 
-    // printf("Cost in Watts = %f \n", wSpendDaily);
+     printf("Cost in Watts = %.1f \n", wattsDay);
     printf("The power consumption of your electrical appliance in %.2f hours/day will consume %f KWh daily. \n", dailyUse, consumptionDay);
     printf("Consuming energy %.2f hours/day will give you a cost of R$ %.2f per day. \n", dailyUse, spendAmountDay);
-    // printf("Cost in Watts = %f \n", wSpendMonthly);
+     printf("Cost in Watts = %.1f \n", wattsMonth);
     printf("The power consumption of your electrical appliance in %.2f hours/day will consume %f KWh monthly. \n", dailyUse, consumptionMonth);
     printf("Consuming energy %.2f hours/day will give you a cost of R$ %.2f per month. \n", dailyUse, spendAmountMonth);
-    // printf("Cost in Watts = %f \n", wSpendYearly);
+     printf("Cost in Watts = %.1f \n", wattsYear);
     printf("The power consumption of your electrical appliance in %.2f hours/day will consume %f KWh yearly. \n", dailyUse, consumptionYear);
     printf("Consuming energy %.2f hours/day will give you a cost of R$ %.2f per year. \n", dailyUse, spendAmountYear);
 
