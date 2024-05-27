2a8b1efee7a4029af082a613ebdef4b300f3f8d3
macedolg <dev.macedolg@gmail.com>
2024-05-27 00:00:27 -0300

    [Finish] Exercício 6 Aula 3.

--- Aula3/functionaryPayRaise.c
+++ Aula3/functionaryPayRaise.c
@@ -1,3 +1,25 @@
 //
 // Created by macedolg on 5/26/24.
 //
+
+#include <stdio.h>
+
+int main(void) {
+
+    double salary, Increase, finalSalary;
+
+    printf("Insert your Initial Salary: \n $");
+    scanf("%lf", &salary);
+
+    Increase = 0.25;
+
+    double salaryIncrease = salary * Increase;
+
+    finalSalary = salary + salaryIncrease;
+
+    int percentualIncrease = Increase * 100;
+
+    printf("After increasing %d%%, your salary is $ %.2lf", percentualIncrease, finalSalary);
+    return 0;
+
+}
