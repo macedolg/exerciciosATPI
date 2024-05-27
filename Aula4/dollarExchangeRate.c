2fd9588ec36bf7e709042334dc1291ed1cd57591
macedolg <dev.macedolg@gmail.com>
2024-05-27 10:17:01 -0300

    [Finish] Exercício 5 Aula 4.

--- Aula4/dollarExchangeRate.c
+++ Aula4/dollarExchangeRate.c
@@ -1,3 +1,22 @@
 //
 // Created by macedolg on 27/05/24.
 //
+
+#include <stdio.h>
+
+int main(void) {
+
+    double BRL, dollarExchange;
+
+    printf("Enter the value of BRL that you want to convert: \nR$");
+    scanf("%lf", &BRL);
+
+    printf("Enter the current exchange rate: \n$");
+    scanf("%lf", &dollarExchange);
+
+    double USD = BRL * dollarExchange;
+
+    printf("\nBased on the current exchange rate, coverting R$%.2lf to US dollars results in $%.2lf.\n ", BRL, USD);
+
+    return 0;
+}
