25e00fe6448652d9605389e61c30663cbac1d0c0
macedolg <dev.macedolg@gmail.com>
2024-05-26 23:11:54 -0300

    [Finish] Exercício 5 Aula 3.

--- Aula3/finalPriceCalc.c
+++ Aula3/finalPriceCalc.c
@@ -1,3 +1,22 @@
 //
 // Created by macedolg on 5/26/24.
 //
+
+#include <stdio.h>
+
+int main(void) {
+    double initialPrice, discountRate, discountAmount, finalPrice;
+
+    printf("Enter the initial price of the product in $: \n");
+    scanf("%lf", &initialPrice);
+
+    discountRate = 0.12;
+
+    discountAmount = initialPrice * discountRate;
+
+    finalPrice = initialPrice - discountAmount;
+
+    printf("\nThe final price of the product: $%.2lf\n", finalPrice);
+
+    return 0;
+}
